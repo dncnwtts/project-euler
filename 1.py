871c371dcb422feb99f08793083f403c87a9ac06
Duncan <duncanjosephwatts@gmail.com>
2014-07-01 10:49:47 -0400

    Got problem 1.

--- 1.py
+++ 1.py
@@ -2,3 +2,26 @@
 # multiples is 23.
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
+
+def multiples(n,k):
+	multiples = []
+	i = 1
+	while i < n:
+		if k*i < n:
+			multiples.append(k*i)
+			i += 1
+		else:
+			return multiples
+
+m3 = multiples(10,3)
+m5 = multiples(10,5)
+mults = m3 + m5
+answer = sum(set(mults))
+
+assert answer == 23, "Test case failed, I got {0}".format(answer)
+
+m3 = multiples(1000,3)
+m5 = multiples(1000,5)
+mults = m3 + m5
+answer = sum(set(mults))
+print(answer)
